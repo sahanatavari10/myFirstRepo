288a828730437be414094192b705c3220d033169
sahanatavari10 <tavarisahana10@gmail.com>
2023-09-09 12:36:04 +0530

    Stack ADT

---
+++ stackadt.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAX 10
+
+typedef struct
+{
+    /* data */
+    int mystack[MAX];
+    int top;
+} stk;
+
+void push(stk *s, int data)
+{
+    if (s->top == MAX)
+    {
+        printf("Stack overflow");
+        return;
+    }
+    s->mystack[++s->top] = data;
+    printf("\n%d pushed", s->mystack[s->top]);
+    return;
+}
+
+int pop(stk *s)
+{
+    if (s->top == -1)
+    {
+        printf("Stack underflow");
+        return -1;
+    }
+    return s->mystack[s->top--];
+}
+
+void peek(stk *s)
+{
+    printf("\n%d top element", s->mystack[s->top]);
+}
+
+void display(stk s)
+{
+    printf("\n");
+    for (int i = 0; i <= s.top; i++)
+    {
+        printf(" %d", s.mystack[i]);
+    }
+}
+
+int main()
+{
+    stk s;
+    s.top = -1; // initializing the stack
+    push(&s, 1);
+    push(&s, 2);
+    push(&s, 3);
+    display(s);
+    pop(&s);
+    display(s);
+    peek(&s);
+}
